c7bccbc82babba7ad2cda02456e75f063f091545
Nixon2223 <n.nixon@hotmail.co.uk>
2022-02-22 16:46:23 +0000

    first commit

---
+++ UserFolderFile/src/test/java/com/codeclan/example/UserFolderFile/UserFolderFileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.UserFolderFile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserFolderFileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
