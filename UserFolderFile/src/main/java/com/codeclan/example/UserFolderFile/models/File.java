ee58e78b11409888cd9ee616492af45287395086
Nixon2223 <n.nixon@hotmail.co.uk>
2022-02-22 17:03:06 +0000

    models setup

--- UserFolderFile/src/main/java/com/codeclan/example/UserFolderFile/models/File.java
+++ UserFolderFile/src/main/java/com/codeclan/example/UserFolderFile/models/File.java
@@ -3,5 +3,36 @@ package com.codeclan.example.UserFolderFile.models;
 public class File {
 
     private String name;
-    private 
+    private String extension;
+    private int size;
+
+    public File(String name, String extension, int size) {
+        this.name = name;
+        this.extension = extension;
+        this.size = size;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getExtension() {
+        return extension;
+    }
+
+    public void setExtension(String extension) {
+        this.extension = extension;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
 }
